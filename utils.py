88e288817d0457b8815c4d33c9d7851770a4e0b8
sunil1264 <104348939+sunil1264@users.noreply.github.com>
2023-09-23 14:11:09 +0530

    Update utils.py

--- utils.py
+++ utils.py
@@ -382,7 +382,7 @@ async def get_shortlink(link):
     https = link.split(":")[0]
     if "http" == https:
         https = "https"
-     # link = link.replace("http", https)
+      link = link.replace("http", https)
     url = f'https://shrinkme.io/api'
    params = {'api': URL_SHORTNER_WEBSITE_API,
              'url': link,
